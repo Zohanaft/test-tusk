c7e18663b91a80eef5283d3c7b4feaf36919d863
gaydukd <gayduk@gayduk.websm.io>
2019-01-15 10:36:14 +0300

    fixed

--- index.php
+++ index.php
@@ -1,27 +1,25 @@
 <?php
-
+ 
 use App\Core\Router;
-
-function debug($param) {
-?>
-<div style ="background: white;"><pre>
-<?
-    var_dump($param);
-?>
-</div></pre>
-<?php
-
+ 
+function debug($arg) {
+    ?>
+        <div style="background: white;"><pre>
+            <?php
+                echo var_dump($arg);
+            ?>
+        </pre></div>
+    <?php
 }
-
+ 
 spl_autoload_register(function($class) {
     $class = str_replace('\\', '/', $class . '.php');
     if (file_exists($class)) {
         require $class;
     }
-    else {
-        echo "can't open";
-    }
 });
  
 $router = new Router();
-$router->run();?>
+$router->run();
+ 
+?>
